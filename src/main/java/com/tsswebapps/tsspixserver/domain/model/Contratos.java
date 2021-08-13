b2bf1d2d15e5474457778defdbb6fc6481a21090
xthiagosant <uzmaki_thiago@outlook.com>
2021-08-12 21:39:39 -0300

    criado classe de contratos

---
+++ src/main/java/com/tsswebapps/tsspixserver/domain/model/Contratos.java
@@ -0,0 +1,41 @@
+package com.tsswebapps.tsspixserver.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Contratos {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	@OneToOne
+	@JoinColumn(nullable = false, referencedColumnName = "id")
+	private Parceiros parceiro;
+	
+	@Column(nullable = false)
+	private String validade;
+	
+	@Column(nullable = false)
+	private String keyPub;
+	
+	@Column(nullable = false)
+	private String keyPri;
+	
+	@Column(nullable = false)
+	private String token;
+	
+	@Column(nullable = false)
+	private String refrshToken;
+}
